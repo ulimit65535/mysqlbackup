263ba413fdec222f604a94ec6e383174bc40e377
ulimit65535 <2510823550@qq.com>
2021-05-11 11:00:32 +0800

    基本完成

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import logging
 import datetime
 import tarfile
 import shutil
+import time
+
+import paramiko
 
 logging_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'run.log')
 logging.basicConfig(filename=logging_file, level=logging.INFO,
@@ -12,10 +15,16 @@ logging.basicConfig(filename=logging_file, level=logging.INFO,
                     datefmt='%Y-%m-%d %H:%M')
 
 
+def notify():
+    pass
+
+
 class MysqlBackup(object):
     """Mysql备份、异地备份、发送邮件"""
-    def __init__(self):
-        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.ini')
+    def __init__(self, config_file=None):
+        if not config_file:
+            config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.ini')
+            assert os.path.exists(config_file), '未找到config.ini配置文件:{}'.format(config_file)
         config = configparser.ConfigParser()
         config.read(config_file, encoding='utf-8')
 
@@ -24,13 +33,13 @@ class MysqlBackup(object):
         self.mysql_user = config.get('BACKUP', 'user')
         self.mysql_password = config.get('BACKUP', 'password')
         self.local_backup_path = config.get('BACKUP', 'backup_path')
-        self.local_reserve_days = config.get('BACKUP', 'reserve_days')
+        self.local_reserve_days = int(config.get('BACKUP', 'reserve_days'))
         try:
             self.mysql_host = config.get('BACKUP', 'host')
         except configparser.NoOptionError:
             self.mysql_host = 'localhost'
         try:
-            self.mysql_port = config.get('BACKUP', 'port')
+            self.mysql_port = int(config.get('BACKUP', 'port'))
         except configparser.NoOptionError:
             self.mysql_port = 3306
         try:
@@ -46,35 +55,14 @@ class MysqlBackup(object):
         except configparser.NoOptionError:
             self.my_cnf = '/etc/my.cnf'
 
-        self.is_send_mail = False
-        if config.has_section('MAIL'):
-            self.smtp_host = config.get('MAIL', 'host')
-            self.smtp_port = config.get('MAIL', 'port')
-            self.smtp_user = config.get('MAIL', 'user')
-            self.smtp_password = config.get('MAIL', 'password')
-            self.smtp_from = config.get('MAIL', 'from')
-
-            self.receivers = []
-            try:
-                to = config.get('MAIL', 'to')
-            except configparser.NoOptionError:
-                logging.info('未配置收件人，将不会发送邮件')
-            else:
-                for receiver in to.split(','):
-                    self.receivers.append(receiver.strip())
-                if len(self.receivers):
-                    self.is_send_mail = True
-        else:
-            logging.info('未发现SMTP配置，将不会发送邮件')
-
         if config.has_section('REMOTE'):
             self.is_remote_backup = True
             self.ssh_host = config.get('REMOTE', 'host')
             self.ssh_user = config.get('REMOTE', 'user')
             self.remote_backup_path = config.get('REMOTE', 'backup_path')
-            self.remote_reserve_days = config.get('REMOTE', 'reserve_days')
+            self.remote_reserve_days = int(config.get('REMOTE', 'reserve_days'))
             try:
-                self.ssh_port = config.get('REMOTE', 'port')
+                self.ssh_port = int(config.get('REMOTE', 'port'))
             except configparser.NoOptionError:
                 self.ssh_port = 22
             try:
@@ -87,12 +75,20 @@ class MysqlBackup(object):
             logging.info('未发现REMOTE配置，将不会进行异地备份')
 
         # 创建本次备份文件夹
-        self.backup_dir = str(datetime.datetime.now().strftime('%Y%m%d%H%M%S'))
-        os.makedirs(os.path.join(self.local_backup_path, self.backup_dir))
+        self.backup_dir = os.path.join(self.local_backup_path, str(datetime.datetime.now().strftime('%Y%m%d%H%M%S')))
+        os.makedirs(self.backup_dir)
+        self.backup_file = self.backup_dir + '.tar.gz'
 
-    def structure_backup(self):
+    def test(self):
+        dump_file = os.path.join(self.backup_dir, 'database.sql')
+        os.system('touch {}'.format(dump_file))
+
+    def structure_backup(self, backup_dir=None):
         """利用mysqldump备份表结构"""
-        dump_file = os.path.join(self.local_backup_path, self.backup_dir, 'database.sql')
+        if not backup_dir:
+            backup_dir = self.backup_dir
+
+        dump_file = os.path.join(backup_dir, 'database.sql')
         result = os.system('{} -u{} -p{} -h {} -P {} --all-databases -d --set-gtid-purged=OFF> {}'.format(
             self.mysqldump_path,
             self.mysql_user,
@@ -107,9 +103,12 @@ class MysqlBackup(object):
         else:
             return True
 
-    def full_backup(self):
+    def full_backup(self, backup_dir):
         """利用percona工具整库备份"""
-        dump_dir = os.path.join(self.local_backup_path, self.backup_dir, 'databases')
+        if not backup_dir:
+            backup_dir = self.backup_dir
+
+        dump_dir = os.path.join(backup_dir, 'databases')
         result = os.system('{} --defaults-file={} --host={} --port={} --user={} --password={} --no-timestamp {}'.format(
             self.innobackupex_path,
             self.my_cnf,
@@ -125,39 +124,95 @@ class MysqlBackup(object):
         else:
             return True
 
-    def compress(self):
+    def compress(self, source_dir=None, target_file=None):
+        if not source_dir:
+            source_dir = self.backup_dir
+        if not target_file:
+            target_file = self.backup_file
         """压缩备份文件夹"""
-        output_filename = os.path.join(self.local_backup_path, self.backup_dir + '.tar.gz')
-        source_dir = os.path.join(self.local_backup_path, self.backup_dir)
         try:
-            with tarfile.open(output_filename, "w:gz") as tar:
+            with tarfile.open(target_file, "w:gz") as tar:
                 tar.add(source_dir, arcname=os.path.basename(source_dir))
             shutil.rmtree(source_dir)
-            return True
+            return target_file
         except Exception as e:
             logging.error('压缩备份文件夹失败:{}'.format(e))
-            return False
+            return None
 
-    def local_clean(self):
+    def local_clean(self, backup_path=None, reserve_days=None):
         """清理本地历史备份"""
-        pass
+        if not backup_path:
+            backup_path = self.local_backup_path
+        if not reserve_days:
+            reserve_days = self.local_reserve_days
+        clean_file_list = []
+        now = time.time()
+        reserve_min = reserve_days * 24 * 60
+        for file_name in os.listdir(backup_path):
+            if file_name.endswith('.tar.gz'):
+                file_path_abs = os.path.join(backup_path, file_name)
+                exist_min = int((now - os.path.getmtime(file_path_abs)) / 60)
+                if exist_min > reserve_min:
+                    try:
+                        os.remove(file_path_abs)
+                    except Exception as e:
+                        logging.error('删除本地备份文件失败:{}'.format(e))
+                    else:
+                        logging.info('已删除过期备份文件:{}'.format(file_name))
+                        clean_file_list.append(file_name)
+        return clean_file_list
 
-    def remote_clean(self):
-        """远端历史备份清理"""
-        pass
-
-    def remote_backup(self):
+    def remote_backup_and_clean(self, backup_file=None, remote_path=None, reserve_days=None):
         """异地备份"""
-        pass
+        if not self.is_remote_backup:
+            return None
+        if not backup_file:
+            backup_file = self.backup_file
+        if not remote_path:
+            remote_path = self.remote_backup_path
+        if not reserve_days:
+            reserve_days = self.remote_reserve_days
 
-    def mail(self):
-        """发送邮件通知"""
-        pass
+        try:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(self.ssh_host, self.ssh_port, self.ssh_user, self.ssh_password)
+            sftp = paramiko.SFTPClient.from_transport(ssh.get_transport())
+        except Exception as e:
+            logging.error('ssh连接远程服务器失败:{}'.format(e))
+            return None
+
+        try:
+            sftp.put(backup_file, os.path.join(remote_path, backup_file))
+        except Exception as e:
+            logging.error('远程传输文件失败:{}'.format(e))
+            return None
+        else:
+            # 清理远程备份文件夹
+            now = time.time()
+            reserve_min = reserve_days * 24 * 60
+            files_attr = sftp.listdir_attr(remote_path)
+            clean_file_list = []
+            for file_attr in files_attr:
+                remote_file_abs = os.path.join(remote_path, file_attr.filename)
+                exist_min = int((now - file_attr.st_mtime) / 60)
+                if exist_min > reserve_min:
+                    try:
+                        sftp.remove(remote_file_abs)
+                    except Exception as e:
+                        logging.error('删除远程备份文件失败:{}'.format(e))
+                    else:
+                        logging.info('已删除远程过期备份文件:{}'.format(remote_file_abs))
+                        clean_file_list.append(file_attr.filename)
+            return clean_file_list
 
     def run(self):
-        self.structure_backup()
+        #self.structure_backup()
         #self.full_backup()
+        self.test()
         self.compress()
+        #self.local_clean()
+        self.remote_backup_and_clean()
 
 
 if __name__ == '__main__':
